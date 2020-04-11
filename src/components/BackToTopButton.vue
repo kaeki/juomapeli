5f8e2a01f2cfe421406fd972f51c9431f75ff4c1
Atte Salo <attesalo@msn.com>
2020-04-11 19:09:17 +0300

    Animate backtotop button

--- src/components/BackToTopButton.vue
+++ src/components/BackToTopButton.vue
@@ -1,12 +1,14 @@
 <template>
-  <button
-    v-if="visible"
-    @click="backToTop"
-    class="back-to-top"
-    title="Takaisin ylös">
-    <span aria-hidden="true" class="emoji">🔝</span>
-    <span class="vis-hidden">Hyppää takaisin sivun yläreunaan</span>
-  </button>
+  <transition name="fade">
+    <button
+      v-if="visible"
+      @click="backToTop"
+      class="back-to-top"
+      title="Takaisin ylös">
+      <span aria-hidden="true" class="emoji">🔝</span>
+      <span class="vis-hidden">Hyppää takaisin sivun yläreunaan</span>
+    </button>
+  </transition>
 </template>
 
 <script>
@@ -22,7 +24,6 @@ export default {
       document.documentElement.scrollTop = 0;
     },
     toggle() {
-      console.log('scroll')
       if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
         this.visible = true;
       } else {
@@ -37,29 +38,36 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  .back-to-top {
-    cursor: pointer;
-    position: fixed;
-    border-radius: 50%;
-    width: 4rem;
-    height: 4rem;
-    right: 1rem;
-    bottom: 2rem;
-    font-size: 2rem;
-    background-color: var(--accent);
-    border: 0;
-    box-shadow: 0 5px 12px rgba(0,0,0, .5);
-    transition: .1s;
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .1s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
 
-    &:hover,
-    &:focus,
-    &:active {
-      background-color: var(--accent-dark);
-    }
+.back-to-top {
+  cursor: pointer;
+  position: fixed;
+  border-radius: 50%;
+  width: 4rem;
+  height: 4rem;
+  right: 1rem;
+  bottom: 2rem;
+  font-size: 2rem;
+  background-color: var(--accent);
+  border: 0;
+  box-shadow: 0 5px 12px rgba(0,0,0, .5);
+  transition: .1s;
 
-    .emoji {
-      color: transparent;
-      text-shadow: 0 0 0 white;
-    }
+  &:hover,
+  &:focus,
+  &:active {
+    background-color: var(--accent-dark);
   }
+
+  .emoji {
+    color: transparent;
+    text-shadow: 0 0 0 white;
+  }
+}
 </style>
