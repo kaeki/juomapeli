670432bd15373207b54300748f4a08ef600cefe2
Atte Salo <attesalo@msn.com>
2020-04-13 13:51:32 +0300

    Add pwa

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from '@/App.vue'
 import router from '@/router'
 import store from '@/store';
 import directives from '@/directives';
+import './registerServiceWorker'
 
 Vue.config.productionTip = false
 
