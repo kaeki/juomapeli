01c5c771273b265ddced7ad36e8cff1a76c31b5c
Atte Salo <attesalo@msn.com>
2020-04-13 20:39:13 +0300

    Remove firebase from demobranch

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,3 @@
-import storage from '../firebase';
-
 const store = {
   debug: true,
   state: {
@@ -80,32 +78,7 @@ const store = {
         }
       }
     ]
-    //this.fetchCardsRequest('card-faces', this.state.cards);
-    //this.fetchCardsRequest('card-backs', this.state.cardBacks);
-  },
-  fetchCardsRequest(ref) {
-    const cardsRef = storage.ref(ref);
-    
-    cardsRef.listAll()
-      .then(res => {
-        return res.items.map(async itemRef => {
-          const src = await itemRef.getDownloadURL();
-          const meta = await itemRef.getMetadata();
-          return { src, meta };
-        })
-      })
-      .then(newCards => {
-        Promise.all(newCards)
-          .then(values => {
-            if (ref === 'card-faces') this.state.cards = values;
-            else this.state.cardBacks = values;
-          });
-      })
-      .catch(error => {
-        this.state.cardsError = true;
-        console.log(error);
-      });
-  },
+  }
 }
 
 export default store;
